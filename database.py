f911ec6d41bb0b3fe6bdc05880b6d803f5673e2e
Miguel Angel Mamani <alpha30379@gmail.com>
2025-11-12 11:21:32 -0400

    fix: modificar datos

--- database.py
+++ database.py
@@ -4,8 +4,8 @@ from tkinter import messagebox
 def coneccion():
     try:
         return psycopg2.connect(
-            host="localhost", database="Proyecto_prueba",
-            user="postgres", password="123")
+            host="localhost", database="ingresar_db",
+            user="ingresar_usuario", password="ingresar_contraseña")
     except Exception as e:
         try: messagebox.showerror("Error de conexión", str(e))
         except: print("Error de conexión:", e)
